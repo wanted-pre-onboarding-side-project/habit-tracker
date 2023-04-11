62169c47477e4ab0578a59d244ff1496f2764b54
Seok-CH <tjr6677@gmail.com>
2023-04-11 18:31:18 +0900

    feat: Header 컴포넌트 생성 및 App.tsx와 연결
    먼저 App.tsx와 Header를 연결 후 작업을 하기 위해  기본적인 Header 컴포넌트 함수를 생성하고 연결하였습니다
    
    issue #30의 기능 개발 과정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import Header from "./components/Header";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,5 @@
+const Header = () => {
+  return <h1>습관리스트</h1>;
+};
+
+export default Header;
