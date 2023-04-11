2c9f3b80c138eba131cc43c36333f2302b8f2125
Seok-CH <tjr6677@gmail.com>
2023-04-11 22:10:34 +0900

    feat: DashBoard 컴포넌트 추가 및 cssd연결
    기본적인 DashBoard 컴포넌트를 생성하고 css를 연결하였습니다. DashBoard는 전체 레이아웃에서 가장 상위 영역을 차지하므로
    App.tsx에 App.module.css에서 전체 레이아웃을 flex 형태로 바꾸고 좌우로 보이게끔 구현하였습니다

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
+import styles from "./App.module.css";
 import Header from "./components/Header";
 import Controller from "./components/Contoller";
 import ProgressBar from "./components/ProgressBar";
+import DashBoard from "./components/DashBoard";
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <Controller />
-      <ProgressBar />
+    <div className={styles.globalContainer}>
+      <div className={styles.leftSection}>
+        <Header />
+        <Controller />
+        <ProgressBar />
+      </div>
+      <DashBoard />
     </div>
   );
 };
---
+++ src/components/DashBoard/index.tsx
@@ -0,0 +1,7 @@
+import styles from "./DashBoard.module.css";
+
+const DashBoard = () => {
+  return <div className={styles.container}>DashBoard</div>;
+};
+
+export default DashBoard;
