9425f4f94e7e4c7c1515ebb5d0eb40c47a3e0bdc
jiyeon <a1919361@gmail.com>
2023-04-13 00:09:12 +0900

    feat: dayjs, react-icons 설치. dayjs설정
    dayjs 한국어 로케일 설정
    주의 첫 요일을 월요일로 업데이트

---
+++ src/setupDayjs.ts
@@ -0,0 +1,18 @@
+import dayjs from 'dayjs';
+import weekday from 'dayjs/plugin/weekday';
+import updateLocale from 'dayjs/plugin/updateLocale';
+
+import 'dayjs/locale/ko';
+
+const setupDayjs = () => {
+  dayjs.extend(updateLocale);
+  dayjs.extend(weekday);
+
+  dayjs.locale('ko');
+
+  dayjs.updateLocale('ko', {
+    weekStart: 1, // 첫 주의 시작을 월요일로 설정
+  });
+};
+
+export default setupDayjs;
