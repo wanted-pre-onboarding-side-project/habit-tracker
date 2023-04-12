86364678c3b40f71af9d150153a18c7d23313e09
Seok-CH <tjr6677@gmail.com>
2023-04-12 20:07:02 +0900

    feat: DashBoardItem 세부기능 추가
    그리고 오늘 할 habit이 없을 때는 습관 추가하기 버튼이 나타나도록 구현하였습니다

--- src/components/DashBoard/DashBoardItem.tsx
+++ src/components/DashBoard/DashBoardItem.tsx
@@ -1,7 +1,26 @@
 import styles from "./DashBoard.module.css";
 
-const DashBoardItem = () => {
-  return <div className={styles.itemBox}>DashBoardItem</div>;
+type DashBoardItemProps = {
+  data: {
+    id: number;
+    title: string;
+    description: string;
+    habitDays: string[];
+    checkDays: string[];
+  };
+};
+
+const DashBoardItem = ({ data }: DashBoardItemProps) => {
+  return (
+    <div className={styles.itemBox}>
+      <h2 className={styles.itemTitle}>{data.title}</h2>
+      <p className={styles.itemDescription}>{data.description}</p>
+      <button className={styles.itemMoreButton}>더보기</button>
+      <button className={styles.itemCheckButton}>
+        {data.checkDays.includes("2023-04-12") ? "취소" : "완료하기"}
+      </button>
+    </div>
+  );
 };
 
 export default DashBoardItem;
--- src/components/DashBoard/index.tsx
+++ src/components/DashBoard/index.tsx
@@ -1,7 +1,17 @@
+import mockdata from "../../mock/data.json";
 import styles from "./DashBoard.module.css";
+import DashBoardItem from "./DashBoardItem";
 
 const DashBoard = () => {
-  return <div className={styles.container}>DashBoard</div>;
+  return (
+    <div className={styles.container}>
+      {mockdata.length === 0 ? (
+        <button>습관 추가하기</button>
+      ) : (
+        mockdata.map((item) => <DashBoardItem key={item.id} data={item} />)
+      )}
+    </div>
+  );
 };
 
 export default DashBoard;
