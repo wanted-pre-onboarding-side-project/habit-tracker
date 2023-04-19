806782f900e086d2c80f4d390b62010881d9094e
sangyeon kim <greyhairchooselife@gmail.com>
2023-04-19 18:13:56 +0900

    refactor: 함수명 변경 getWeek -> getPeriod

--- src/contexts/RecordContextProvider.tsx
+++ src/contexts/RecordContextProvider.tsx
@@ -1,13 +1,13 @@
 import { useState, ReactNode, useCallback } from "react";
-import { getWeek } from "lib/utils/dateUtils";
+import { getPeriod } from "lib/utils/dateUtils";
 import { PeriodContext, PeriodHandleContext } from "./RecordContext";
 
 export const RecordProvider = ({ children }: { children: ReactNode }) => {
-  const [period, setPeriod] = useState(getWeek());
+  const [period, setPeriod] = useState(getPeriod());
 
   const movePeriod = useCallback(
     (direction: "prev" | "next") => {
-      setPeriod(getWeek(period.start, direction === "prev" ? -7 : 7));
+      setPeriod(getPeriod(period.start, direction === "prev" ? -7 : 7));
     },
     [period]
   );
--- src/lib/utils/dateUtils.ts
+++ src/lib/utils/dateUtils.ts
@@ -43,7 +43,7 @@ export const getToday = () => {
   return objectifyDate(now);
 };
 
-export const getWeek = (
+export const getPeriod = (
   currentPeriodStart?: ObjectifiedDate,
   periodOffset?: number
 ) => {
