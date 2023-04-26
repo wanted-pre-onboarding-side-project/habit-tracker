7b30b2aca26ba96872a45ad217ca50aa9d2d79fe
changhwan seok <83488329+chadseok@users.noreply.github.com>
2023-04-26 20:26:51 +0900

    refactor: HabitCard 컴포넌트의 로직 분리
    작업 관심사를 분리하기 위해 HabitCard 컴포넌트의 상태로직을 useHabitCard에 분리하였습니다

--- src/components/Dashboard/HabitCard/HabitCard.tsx
+++ src/components/Dashboard/HabitCard/HabitCard.tsx
@@ -1,17 +1,17 @@
-import { useRef, useEffect, useState } from 'react';
-import { TOGGLE_BY_HEIGHT } from 'constant';
 import styles from './HabitCard.module.css';
+import useHabitCard from './useHabitCard';
 import type { Habit } from 'interface/main';
 
 const HabitCard = ({ habit }: { habit: Habit }) => {
-  const cardRef = useRef<HTMLDivElement>(null);
-  const [isLongToFold, setIsLongToFold] = useState<boolean>(false);
-  const [isFold, setIsFold] = useState<boolean>(true);
-
-  useEffect(() => {
-    if (cardRef.current && cardRef.current.offsetHeight > TOGGLE_BY_HEIGHT)
-      setIsLongToFold(true);
-  }, []);
+  const {
+    cardRef,
+    isLongToFold,
+    isFold,
+    toggleFold,
+    foldButtonContent,
+    toggleComplete,
+    completeButtonContent,
+  } = useHabitCard(habit);
 
   return (
     <div ref={cardRef} className={styles.container}>
@@ -24,21 +24,15 @@ const HabitCard = ({ habit }: { habit: Habit }) => {
         {habit.description}
       </p>
       {isLongToFold && (
-        <button
-          onClick={() => setIsFold((prev) => !prev)}
-          className={styles.foldButton}
-        >
-          {isFold ? '더보기' : '닫기'}
+        <button onClick={toggleFold} className={styles.foldButton}>
+          {foldButtonContent}
         </button>
       )}
-      <button className={styles.completeToggleButton}>완료</button>
+      <button className={styles.completeToggleButton} onClick={toggleComplete}>
+        {completeButtonContent}
+      </button>
     </div>
   );
 };
 
 export default HabitCard;
-
-//  TODO
-//
-//  habit.id랑 오늘 날짜 값으로 record도 가져와야겠네.
-//  habit.id로 dispatch done or undone
---
+++ src/components/Dashboard/HabitCard/useHabitCard.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { useHabitDispatchContext } from 'contexts/HabitContext';
+import { TOGGLE_BY_HEIGHT } from 'constant';
+import { getFormattedString } from 'lib/utils/dateUtils';
+import { getToday } from 'lib/utils/dateUtils';
+import type { Habit } from 'interface/main';
+
+const useHabitCard = (habit: Habit) => {
+  const cardRef = React.useRef<HTMLDivElement>(null);
+  const [isLongToFold, setIsLongToFold] = React.useState<boolean>(false);
+  const [isFold, setIsFold] = React.useState<boolean>(true);
+  const dispatch = useHabitDispatchContext();
+  const foldButtonContent = isFold ? '더보기' : '닫기';
+  const completeButtonContent =
+    habit.recordedDates[getFormattedString(getToday().origin)] === 'completed'
+      ? '취소'
+      : '완료';
+
+  React.useEffect(() => {
+    if (cardRef.current && cardRef.current.offsetHeight > TOGGLE_BY_HEIGHT)
+      setIsLongToFold(true);
+  }, []);
+
+  const toggleFold = () => {
+    setIsFold((prev) => !prev);
+  };
+
+  const toggleComplete = () => {
+    const clonedHabit = { ...habit };
+    clonedHabit.recordedDates[getFormattedString(getToday().origin)] =
+      clonedHabit.recordedDates[getFormattedString(getToday().origin)] ===
+      'inactive'
+        ? 'completed'
+        : 'inactive';
+
+    dispatch({ type: 'UPDATE', payload: clonedHabit });
+  };
+
+  return {
+    cardRef,
+    isLongToFold,
+    isFold,
+    toggleFold,
+    foldButtonContent,
+    toggleComplete,
+    completeButtonContent,
+  };
+};
+
+export default useHabitCard;
