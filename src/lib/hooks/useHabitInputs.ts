a255b95fe03344015f9b13b2648e27e5238addb7
sangyeon kim <greyhairchooselife@gmail.com>
2023-04-27 02:48:44 +0900

    feat: habit inputs 커스텀 훅
    - name, desc, days(요일)와 이 상태들의 setter 핸들러들을 리턴합니다.
    
    - habit의 생성, 수정 시 사용합니다.

---
+++ src/lib/hooks/useHabitInputs.ts
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import { Habit, Day } from 'interface/main';
+
+const useHabitInputs = (
+  initName: string,
+  initDesc: string,
+  initDays: Day[],
+): [
+  Omit<Habit, 'id'>,
+  (event: React.ChangeEvent<HTMLInputElement>) => void,
+  (event: React.ChangeEvent<HTMLTextAreaElement>) => void,
+  (event: React.ChangeEvent<HTMLInputElement>) => void,
+] => {
+  const [name, setName] = useState(initName);
+  const [description, setDescription] = useState(initDesc);
+  const [days, setDays] = useState<Day[]>(
+    initDays.length === 0
+      ? ['월', '화', '수', '목', '금', '토', '일']
+      : initDays,
+  );
+
+  const onChangeName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
+
+  const onChangeDesc = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setDescription(event.target.value);
+  };
+
+  const onChangeDays = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name: selectedDay, checked: isChecked } = event.target;
+    if (isChecked) setDays([...days, selectedDay as Day]);
+    else {
+      const omitted = days.reduce((prev: Day[], curr) => {
+        if (curr === selectedDay) return prev;
+        else return [...prev, curr];
+      }, []);
+      setDays(omitted);
+    }
+  };
+
+  const habitObject = {
+    name,
+    description,
+    days,
+  };
+
+  return [habitObject, onChangeName, onChangeDesc, onChangeDays];
+};
+
+export default useHabitInputs;
