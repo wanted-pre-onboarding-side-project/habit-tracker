e7eaaa73c19724149acb1a1d3869f7ceec1202cb
changhwan seok <83488329+chadseok@users.noreply.github.com>
2023-04-27 01:30:21 +0900

    refactor: Popover 컴포넌트 리팩터링
    기존에는 Popover컴포넌트는 아래 children에 따라 다르게 보여주도록 구현하였지만 Popover가 같은 형태로 계속 쓰인다는 것을 파악하고
    children을 없애고 element들을 직접 작성하였습니다

--- src/components/HabitList/HabitList.tsx
+++ src/components/HabitList/HabitList.tsx
@@ -1,15 +1,10 @@
-import { useModalHandleContext } from 'contexts/ModalContext';
-import { useHabitDispatchContext } from 'contexts/HabitContext';
-import Modal from 'components/Modal/Modal';
 import Popover from 'components/Popover/Popover';
-import Dialog from 'components/Dialog/Dialog';
 import styles from './HabitList.module.css';
 import useHabitList from './useHabitList';
+
 const HabitList = () => {
   const { weekData, habits, getTypeOfButton, getAchieveRate, toggleComplete } =
     useHabitList();
-  const { toggleModal, changeModalComponent } = useModalHandleContext();
-  const dispatch = useHabitDispatchContext();
 
   return (
     <div className={styles.container}>
@@ -27,33 +22,8 @@ const HabitList = () => {
       {habits.map((item) => (
         <div className={styles.gridRow} key={item.id}>
           <div className={styles.gridRowHeading}>
-            {item.name}
-            <Popover>
-              <button
-                onClick={() => {
-                  toggleModal();
-                  changeModalComponent(<Modal habitToUpdate={item} />);
-                }}
-              >
-                Edit Haibt
-              </button>
-              <button
-                onClick={() => {
-                  toggleModal();
-                  changeModalComponent(
-                    <Dialog
-                      onCancel={toggleModal}
-                      onConfirm={() => {
-                        dispatch({ type: 'DELETE', payload: item });
-                        toggleModal();
-                      }}
-                    />,
-                  );
-                }}
-              >
-                Delete Habit
-              </button>
-            </Popover>
+            <span>{item.name}</span>
+            <Popover habit={item} />
           </div>
           <div className={styles.gridRowMain}>
             {weekData.map((day) => (
--- src/components/Popover/Popover.tsx
+++ src/components/Popover/Popover.tsx
@@ -1,14 +1,58 @@
 import React from 'react';
+import { AiOutlineMore } from 'react-icons/ai';
+import { useModalHandleContext } from 'contexts/ModalContext';
+import { useHabitDispatchContext } from 'contexts/HabitContext';
+import Modal from 'components/Modal/Modal';
+import Dialog from 'components/Dialog/Dialog';
+import styles from './Popover.module.css';
+import type { Habit } from 'interface/main';
 
-const Popover = ({ children }: { children: React.ReactNode }) => {
+const HabitPopover = ({ habit }: { habit: Habit }) => {
   const [isOpen, setIsOpen] = React.useState(false);
+  const { toggleModal, changeModalComponent } = useModalHandleContext();
+  const dispatch = useHabitDispatchContext();
 
   return (
-    <div onClick={() => setIsOpen((prev) => !prev)}>
-      <button>:</button>
-      {isOpen && <div>{children}</div>}
+    <div className={styles.container}>
+      <button
+        className={styles.toggleButton}
+        onClick={() => setIsOpen((prev) => !prev)}
+        data-open={isOpen}
+      >
+        <AiOutlineMore size={24} />
+      </button>
+      {isOpen && (
+        <div className={styles.innerContainer}>
+          <button
+            onClick={() => {
+              toggleModal();
+              setIsOpen((prev) => !prev);
+              changeModalComponent(<Modal habitToUpdate={habit} />);
+            }}
+          >
+            Edit Habit
+          </button>
+          <button
+            onClick={() => {
+              toggleModal();
+              setIsOpen((prev) => !prev);
+              changeModalComponent(
+                <Dialog
+                  onCancel={toggleModal}
+                  onConfirm={() => {
+                    dispatch({ type: 'DELETE', payload: habit });
+                    toggleModal();
+                  }}
+                />,
+              );
+            }}
+          >
+            Delete Habit
+          </button>
+        </div>
+      )}
     </div>
   );
 };
 
-export default Popover;
+export default HabitPopover;
