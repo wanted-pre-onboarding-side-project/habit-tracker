bee417f1f4545ccdeac448ff9f090fab4c298a7b
sangyeon kim <greyhairchooselife@gmail.com>
2023-04-26 20:25:47 +0900

    refactor: module.css 적용 안된 것 마저 바꿈
    - HabitTracker.css => HabitTracker.module.css

--- src/HabitTracker.tsx
+++ src/HabitTracker.tsx
@@ -1,13 +1,13 @@
-import './HabitTracker.css';
 import Header from 'components/Header';
 import Controller from 'components/controller/Controller';
 import DashBoard from 'components/dashBoard/Dashboard';
 import TempHabitCreateForm from 'components/temporary/TempHabitCreateForm';
 import TempHabitList from 'components/temporary/TempHabitList';
+import styles from './HabitTracker.module.css';
 
 const HabitTracker = () => {
   return (
-    <div className="HabitTracker HabitTrackerLayout">
+    <div className={`${styles.HabitTracker} HabitTrackerLayout`}>
       <Header />
       <Controller />
       <DashBoard />
