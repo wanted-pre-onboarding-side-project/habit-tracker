ad6d58221bcf4237b20944e568a2503395309945
changhwan seok <83488329+chadseok@users.noreply.github.com>
2023-04-27 10:55:28 +0900

    style: Dialog ui 수정

--- src/components/Dialog/Dialog.tsx
+++ src/components/Dialog/Dialog.tsx
@@ -10,10 +10,12 @@ const Dialog = ({
   return (
     <div className={styles.overlay}>
       <div className={styles.container}>
-        <h3>정말 삭제하시겠습니까?</h3>
-        <p>삭제 후 다시 복구 할 수 없습니다.</p>
-        <button onClick={onCancel}>취소</button>
-        <button onClick={onConfirm}>확인</button>
+        <h3 className={styles.title}>정말 삭제하시겠습니까?</h3>
+        <p className={styles.description}>삭제 후 다시 복구 할 수 없습니다.</p>
+        <div className={styles.buttonContainer}>
+          <button onClick={onCancel}>취소</button>
+          <button onClick={onConfirm}>확인</button>
+        </div>
       </div>
     </div>
   );
