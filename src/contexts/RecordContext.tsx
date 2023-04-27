9b92c5b3cd380c666e66cae75bfe852d56e56506
sangyeon kim <greyhairchooselife@gmail.com>
2023-04-28 06:45:13 +0900

    feat: record 상태값 컨텍스트 공급 시작 외 1
    - useLocalStorageReducer 훅을 사용했습니다.
    
    - action type 'INIT'을 활성화했습니다.
      - useEffect 훅을 사용하여 period, habit 상태값에 의존하게 했습니다.
      - 이때, period가 바뀌거나 habit이 추가 생성 되어 records.length와 길이가 달라지면 실행됩니다.

--- src/contexts/RecordContext.tsx
+++ src/contexts/RecordContext.tsx
@@ -3,9 +3,11 @@ import type {
   PeriodContextType,
   PeriodHandleContextType,
 } from 'interface/context';
+import type { WeekRecord } from 'interface/main';
 
 export const PeriodContext = createContext<PeriodContextType>(null);
 export const PeriodHandleContext = createContext<PeriodHandleContextType>(null);
+export const RecordContext = createContext<WeekRecord[]>([]);
 
 export const usePeriodContext = () => {
   const context = useContext(PeriodContext);
@@ -19,6 +21,14 @@ export const usePeriodHandleContext = () => {
   const context = useContext(PeriodHandleContext);
   if (!context)
     throw new Error('<PeriodHandleContext.Provider>가 제공되지 않았습니다.');
+
+  return context;
+};
+
+export const useRecordContext = () => {
+  const context = useContext(RecordContext);
+  if (!context)
+    throw new Error('<RecordContext.Provider>가 제공되지 않았습니다.');
 
   return context;
 };
--- src/contexts/RecordContextProvider.tsx
+++ src/contexts/RecordContextProvider.tsx
@@ -1,9 +1,17 @@
-import { useState, ReactNode, useCallback } from 'react';
+import { useState, useEffect, ReactNode, useCallback } from 'react';
 import { getLatestPeriod, getChangedPeriod } from 'lib/utils/dateUtils';
+import useLocalStorageReducer from 'lib/hooks/useLocalStorageReducer';
+import { recordReducer } from 'lib/reducers/recordReducer';
+import { useHabitsContext } from './HabitContext';
 import { PeriodContext, PeriodHandleContext } from './RecordContext';
 
 export const RecordProvider = ({ children }: { children: ReactNode }) => {
+  const habits = useHabitsContext();
   const [period, setPeriod] = useState(getLatestPeriod());
+  const { state: records, dispatch } = useLocalStorageReducer(
+    'records',
+    recordReducer,
+  );
 
   const movePeriod = useCallback(
     (direction: 'prev' | 'next') => {
@@ -11,6 +19,10 @@ export const RecordProvider = ({ children }: { children: ReactNode }) => {
     },
     [period],
   );
+
+  useEffect(() => {
+    dispatch({ type: 'INIT', value: { habits } });
+  }, [period, dispatch, habits]);
 
   return (
     <PeriodContext.Provider value={period}>
