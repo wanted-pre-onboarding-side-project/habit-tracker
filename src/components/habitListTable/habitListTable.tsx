18db8620cbe110538eb406217a53c11c9495f7a7
sangyeon kim <greyhairchooselife@gmail.com>
2023-04-28 19:48:48 +0900

    feat: 오늘 요일에 하이라이트

--- src/components/habitListTable/habitListTable.tsx
+++ src/components/habitListTable/habitListTable.tsx
@@ -3,9 +3,23 @@ import TableBody from './parts/TableBody';
 import styles from './HabitListTable.module.css';
 
 const HabitListTable = () => {
+  const COLUMN_LENGTH = 10; // total table column length
+  const WEEK_COLUMN_OFFSET = 2; //  Mon ~ Sun column exist after
+  const columnStyle = (
+    <colgroup>
+      {Array.from(Array(COLUMN_LENGTH).keys()).map((key, idx) => {
+        const TODAY_INDEX = WEEK_COLUMN_OFFSET + new Date().getDay();
+        if (idx + 1 === TODAY_INDEX)
+          return <col key={key} style={{ backgroundColor: 'red' }} />;
+        else return <col key={key} />;
+      })}
+    </colgroup>
+  );
+
   return (
     <div className={`${styles.HabitListTable} HabitListTableLayout`}>
       <table>
+        {columnStyle}
         <TableHead />
         <TableBody />
       </table>
