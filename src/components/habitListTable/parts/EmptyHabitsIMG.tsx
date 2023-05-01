c7cb4bb7f23b88fd75acbc3beac15ba9836e2129
sangyeon kim <greyhairchooselife@gmail.com>
2023-05-01 17:04:43 +0900

    refactor: tBody 태그 아래 img가 올 수 없는 문제

--- src/components/habitListTable/parts/EmptyHabitsIMG.tsx
+++ src/components/habitListTable/parts/EmptyHabitsIMG.tsx
@@ -1,5 +1,9 @@
 const EmptyImageRender = () => (
-  <img src={process.env.PUBLIC_URL + '/empty_note.jpeg'} alt="empty_note" />
+  <tr>
+    <td>
+      <img src={process.env.PUBLIC_URL + '/empty_note.jpeg'} alt="empty_note" />
+    </td>
+  </tr>
 );
 
 export default EmptyImageRender;
