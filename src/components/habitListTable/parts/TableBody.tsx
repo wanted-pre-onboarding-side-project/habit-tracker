fc89800ce10378291c8460a8f801073bb6afcb7d
sangyeon kim <greyhairchooselife@gmail.com>
2023-05-01 15:58:21 +0900

    refactor: habit List에서 필요한 목록만 보여주기
    - 기존: 모든 habits가 리스트에 표시됨.
    
    - 변경: period와 habit의 createdAt을 비교하여 기간 내에 만들어진
      habit들만 표시

--- src/components/habitListTable/parts/TableBody.tsx
+++ src/components/habitListTable/parts/TableBody.tsx
@@ -3,6 +3,7 @@ import { useTooltipContext } from 'contexts/ModalContext';
 import { useTooltipHandleContext } from 'contexts/ModalContext';
 import { useRecordContext } from 'contexts/RecordContext';
 import { useRecordHandleContext } from 'contexts/RecordContext';
+import { usePeriodContext } from 'contexts/RecordContext';
 import { getAcheiveRecord } from 'lib/utils/recordsParser';
 import Tooltip from 'components/modals/modalForms/Tooltip';
 import { ALL_DAYS } from 'constant';
@@ -13,6 +14,7 @@ import type { Habit, Day } from 'interface/main';
 
 const TableBody = () => {
   const habits = useHabitsContext();
+  const period = usePeriodContext();
   const records = useRecordContext();
   const { checkDay, unCheckDay } = useRecordHandleContext();
   const tooltipId = useTooltipContext();
@@ -32,36 +34,41 @@ const TableBody = () => {
     else unCheckDay(id, dayName);
   };
 
+  const isWithinPeriod = (createdAt: number): boolean =>
+    createdAt <= period.start.origin.getTime();
+
   return (
     <tbody>
       {!habits.length && <EmptyImageRender />}
-      {habits.map(({ id, name, days }, idx) => (
-        <tr key={id}>
-          <td onClick={() => setTooltipId(id)}>{name}</td>
-          <td>{tooltipId === id && <Tooltip />}</td>
-          {ALL_DAYS.map((DAY) => (
-            <td key={DAY}>
-              {days.includes(DAY) && (
-                <input
-                  className={
-                    isFutureDay(DAY) ? `${styles.DisabledCheckbox}` : ``
-                  }
-                  type="checkbox"
-                  name={DAY}
-                  defaultChecked={isCheckedDay(id, DAY)}
-                  onChange={(e) => onChangeCheckbox(e, id)}
-                  disabled={isFutureDay(DAY)}
-                />
-              )}
+      {habits
+        .filter(({ createdAt }) => isWithinPeriod(createdAt))
+        .map(({ id, name, days }, idx) => (
+          <tr key={id}>
+            <td onClick={() => setTooltipId(id)}>{name}</td>
+            <td>{tooltipId === id && <Tooltip />}</td>
+            {ALL_DAYS.map((DAY) => (
+              <td key={DAY}>
+                {days.includes(DAY) && (
+                  <input
+                    className={
+                      isFutureDay(DAY) ? `${styles.DisabledCheckbox}` : ``
+                    }
+                    type="checkbox"
+                    name={DAY}
+                    defaultChecked={isCheckedDay(id, DAY)}
+                    onChange={(e) => onChangeCheckbox(e, id)}
+                    disabled={isFutureDay(DAY)}
+                  />
+                )}
+              </td>
+            ))}
+            <td>
+              {`${getAcheiveRecord(records[idx]?.checkedDays).checked} / ${
+                getAcheiveRecord(records[idx]?.checkedDays).total
+              }`}
             </td>
-          ))}
-          <td>
-            {`${getAcheiveRecord(records[idx]?.checkedDays).checked} / ${
-              getAcheiveRecord(records[idx]?.checkedDays).total
-            }`}
-          </td>
-        </tr>
-      ))}
+          </tr>
+        ))}
     </tbody>
   );
 };
