3728c84d7d12e8a83a2103c862f4c5d297e8cc4f
sangyeon kim <greyhairchooselife@gmail.com>
2023-05-01 19:35:41 +0900

    style: 전체 grid 요소들이 고정 폭을 가지도록 수정

--- src/components/controller/Controller.tsx
+++ src/components/controller/Controller.tsx
@@ -2,10 +2,11 @@ import AddHabitBtn from './parts/AddHabitBtn';
 import NavBtn from './parts/NavBtn';
 import Period from './parts/Period';
 import ProgressBar from './parts/ProgressBar';
+import styles from './Controller.module.css';
 
 const Controller = () => {
   return (
-    <div className="HabitControllerLayout">
+    <div className={`${styles.HabitController} HabitControllerLayout`}>
       <AddHabitBtn />
       <NavBtn />
       <Period />
