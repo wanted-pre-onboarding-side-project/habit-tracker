78689d6f935fe1b2eb770a56ac3e7f9d6ce2729b
changhwan seok <83488329+chadseok@users.noreply.github.com>
2023-06-04 14:07:31 +0900

    refactor: Dashboard 상태 관련 로직 분리
    Dashboard.tsx에 있던 상태관련 로직을 useDashboard.tsx파일로 옮겼습니다

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,16 +1,9 @@
-import { useHabitStateContext } from 'contexts/HabitContext';
-import { getDayword } from 'lib/helpers/date';
-import { getRoutineByDate } from 'lib/helpers/habit';
 import styles from './Dashboard.module.css';
 import HabitCard from './HabitCard/HabitCard';
+import useDashboard from './useDashboard';
 
 const DashBoard = () => {
-  const habits = useHabitStateContext();
-  const todayHabits = habits.filter((habit) => {
-    const todayDate = new Date();
-    const routine = getRoutineByDate(habit.routineList, todayDate);
-    return routine?.includes(getDayword(todayDate)) ? true : false;
-  });
+  const todayHabits = useDashboard();
 
   return (
     <div className={styles.container}>
